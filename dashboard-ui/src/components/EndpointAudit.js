9af87e27619c077d317bac8cb9cfe4411410a342
vycodin <kbyin2@gmail.com>
2023-11-23 13:05:28 -0800

    endpoint audit fixes

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -5,17 +5,17 @@ export default function EndpointAudit(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [log, setLog] = useState(null);
     const [error, setError] = useState(null)
-    const [index, setIndex] = useState(null);
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
+    const [index, setIndex] = useState(null);
 
 
     const getAudit = () => {
-        setIndex(rand_val)
         fetch(`http://ky-3855.westus3.cloudapp.azure.com:8110/${props.endpoint}?index=${index}`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Audit Results for " + props.endpoint)
                 setLog(result);
+                setIndex(rand_val);
                 setIsLoaded(true);
                 console.log(index);
             },(error) =>{
